72246438377554414e26099fb39d4ec8fdfa0b5b
Chris Cox <Chris@Adamo>
2012-10-26 15:43:09 +0100

    Cleaned up header, added HTML5 structure and wrapper for grid

--- footer.php
+++ footer.php
@@ -11,23 +11,24 @@
  */
 ?>
 
-<?php
-	/* A sidebar in the footer? Yep. You can can customize
-	 * your footer with four columns of widgets.
-	 */
-	get_sidebar( 'footer' );
-?>
+    <?php
+            /* A sidebar in the footer? Yep. You can can customize
+             * your footer with four columns of widgets.
+             */
+            get_sidebar( 'footer' );
+    ?>
 
-			<a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-			<a href="http://wordpress.org/" title="Semantic Personal Publishing Platform" rel="generator">Proudly powered by WordPress </a>
+                            <a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+                            <a href="http://wordpress.org/" title="Semantic Personal Publishing Platform" rel="generator">Proudly powered by WordPress </a>
 
-<?php
-	/* Always have wp_footer() just before the closing </body>
-	 * tag of your theme, or you will break many plugins, which
-	 * generally use this hook to reference JavaScript files.
-	 */
+    <?php
+            /* Always have wp_footer() just before the closing </body>
+             * tag of your theme, or you will break many plugins, which
+             * generally use this hook to reference JavaScript files.
+             */
 
-	wp_footer();
-?>
+            wp_footer();
+    ?>
+    </div>
 </body>
 </html>
