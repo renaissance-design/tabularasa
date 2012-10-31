8e7c41ada9528966023e879e8bc0a4bc94873ca0
Chris Cox <chris@renaissance-design.net>
2012-10-31 19:15:57 +0000

    Call htaccess script

--- lib/TabulaRasa.php
+++ lib/TabulaRasa.php
@@ -10,17 +10,25 @@ class TabulaRasa {
     private static $instance;
     public $slug;
 
-    public function __construct() {
+    public function __construct($slug = 'tabularasa') {
         
-        $this->slug = 'tabularasa';
+        $this->slug = $slug;
         
-        add_action('init', array(&$this, 'init'));                    
+        add_action('init', array(&$this, 'init'));  
+        
+        add_action('after_setup_theme', array(&$this, 'setup'));
         
         register_activation_hook(__FILE__, array(&$this, 'activate'));
         
         register_deactivation_hook(__FILE__, array(&$this, 'deactivate'));
         
         register_uninstall_hook(__FILE__, array(&$this, 'uninstall'));
+        
+        if (stristr($_SERVER['SERVER_SOFTWARE'], 'apache') || stristr($_SERVER['SERVER_SOFTWARE'], 'litespeed') !== false) {
+            require_once('htaccess.php');
+            $htaccess = new TabulaRasa_htaccess($this->slug);
+        }
+        
     }
     
     /**
@@ -40,7 +48,7 @@ class TabulaRasa {
      * Hooks to WP init()
      */
     function init() {
-       
+        require_once(locate_template('/lib/widgets.php'));
         if (is_admin()) {
             
         }
@@ -69,6 +77,25 @@ class TabulaRasa {
     function uninstall() {
         
     }
+    
+    /**
+     * Sets theme defaults
+     */
+    function setup() {
+        load_theme_textdomain($this->slug, get_template_directory() . '/lang');
+        
+        register_nav_menus(array(
+            'primary_navigation' => __('Primary Navigation', $this->slug),
+          ));
+        
+        add_theme_support('post-thumbnails');
+        
+        add_theme_support('post-formats', array('aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat'));
+        
+        add_editor_style('css/editor-style.css');
+    }
+    
+    
 }
 
 ?>
