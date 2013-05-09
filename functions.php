c9f69b5bd4e9d6a0437aeb1513f9eca0f560f1a0
Chris Cox <Chris@Adamo>
2013-05-09 16:52:40 +0100

    Added default excerpt length

--- functions.php
+++ functions.php
@@ -23,20 +23,6 @@ function TabulaRasa_setup() {
 
 add_action('init', 'TabulaRasa_setup');
 
-/**
- * Sets the post excerpt length to 40 characters.
- *
- * To override this length in a child theme, remove the filter and add your own
- * function tied to the excerpt_length filter hook.
- *
- * @since Twenty Ten 1.0
- * @return int
- */
-function twentyten_excerpt_length($length) {
-	return 40;
-}
-
-add_filter('excerpt_length', 'twentyten_excerpt_length');
 
 /**
  * Returns a "Continue Reading" link for excerpts
