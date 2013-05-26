db26540c278265e60a4652de1fc2efb72c81d12a
Scott Sampson <scott@greyedservices.co.uk>
2013-05-26 18:45:21 +0200

    Update author.php
    Changed information output, and added/extended information (is what I'm currently using - personally, I feel it's a bit better)

--- author.php
+++ author.php
@@ -21,17 +21,20 @@ get_header(); ?>
 		the_post();
 ?>
 
-				<h1><?php printf( __( 'Author Archives: %s', TabulaRasa::get_textdomain() ), "<a class='url fn n' href='" . get_author_posts_url( get_the_author_meta( 'ID' ) ) . "' title='" . esc_attr( get_the_author() ) . "' rel='me'>" . get_the_author() . "</a>" ); ?></h1>
+	<h1><?php printf( __( 'Author Archives: %s', TabulaRasa::get_textdomain() ), "<a class='url fn n' href='" . get_author_posts_url( get_the_author_meta( 'ID' ) ) . "' title='" . esc_attr( get_the_author() ) . "' rel='me'>" . get_the_author() . "</a>" ); ?></h1>
+	<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'twentyten_author_bio_avatar_size', 60 ) ); ?>
+	<h2><?php printf( __( 'About %s', TabulaRasa::get_textdomain() ), get_the_author() ); ?></h2>
+	
+    <ul class="author_info">
+    <?php if (get_the_author_meta('user_url')  ) { ?>
+        <li><span class="author_info_field_title">Website:</span> <a href="<?php the_author_meta('url'); ?>" rel="me"><?php the_author_meta('url'); ?></a></li>
+        <?php } if (get_the_author_meta('twitter')  ) { ?>
+        <li><span class="author_info_field_title">Twitter:</span> <a href="<?php the_author_meta('twitter'); ?>"><?php the_author_meta('twitter'); ?></a></li>
+        <?php } if (get_the_author_meta('description')  ) { ?>
+        <li><span class="author_info_field_title">Profile:</span> <?php the_author_meta('description'); ?></li>
+        <?php } ?>
+    </ul>
 
-<?php
-// If a user has filled out their description, show a bio on their entries.
-if ( get_the_author_meta( 'description' ) ) : ?>
-
-							<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'twentyten_author_bio_avatar_size', 60 ) ); ?>
-							<h2><?php printf( __( 'About %s', TabulaRasa::get_textdomain() ), get_the_author() ); ?></h2>
-							<?php the_author_meta( 'description' ); ?>
-
-<?php endif; ?>
 
 <?php
 	/* Since we called the_post() above, we need to
