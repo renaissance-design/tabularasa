59e4705e3bf4bedc3505c5ab3f65b9b9b66d9f9c
Chris Cox <chris@renaissance-design.net>
2013-05-28 10:38:37 +0100

    Moved 1 folder down the file structure to hide from Theme Editor, fixed paths

---
+++ lib/classes/TabulaRasa.php
@@ -0,0 +1,486 @@
+<?php
+
+/**
+ * Main class for the theme framework
+ *
+ * @package WordPress
+ * @subpackage TabulaRasa
+ * @since TabulaRasa 1.0
+ */
+class TabulaRasa {
+
+  private static $instance;
+  public $slug;
+  private $htaccess;
+  private $options;
+  private $theme;
+  public $settings;
+  public $dev;
+
+  /**
+   * Constructor for the framework class
+   * 
+   */
+  public function __construct() {
+
+    $this->theme = wp_get_theme();
+
+    $this->slug = sanitize_title_with_dashes($this->theme->name);
+
+    $this->settings = get_option('TabulaRasa_general_settings');
+    
+    $this->dev = array();
+
+    add_action('init', array(&$this, 'init'));
+
+    add_action('after_setup_theme', array(&$this, 'setup'));
+
+    if (stristr($_SERVER['SERVER_SOFTWARE'], 'apache') || stristr($_SERVER['SERVER_SOFTWARE'], 'litespeed') !== false) {
+      require_once(locate_template('/lib/classes/htaccess.php'));
+      $this->htaccess = new TabulaRasa_htaccess($this->slug);
+    }
+
+    require_once(locate_template('/lib/classes/options.php'));
+    $this->options = new TabulaRasa_options($this->slug, $this->theme->name);
+
+    if (!class_exists('SmartMetaBox')) {
+      require_once(locate_template('/lib/classes/meta.php'));
+    }
+
+    add_action('widgets_init', array(&$this, 'register_widgets'));
+  }
+
+  /**
+   * Singleton instance
+   * 
+   * @return object
+   */
+  public static function get_instance() {
+    if (!isset(self::$instance)) {
+      $c = __CLASS__;
+      self::$instance = new $c();
+    }
+    return self::$instance;
+  }
+
+  /**
+   * Returns the theme's textdomain for use in templates
+   * 
+   * @return string
+   */
+  public static function get_textdomain() {
+    return self::$instance->slug;
+  }
+
+  /**
+   * Echo pagination. If used with a custom query, it needs to be passed as an argument. Otherwise it assumes the default $wp_query
+   * 
+   * @param object $query An instance of WP_Query
+   */
+  public static function paginate($query = '') {
+    if (!($query instanceof WP_Query)) {
+      global $wp_query;
+      $query = $wp_query;
+    }
+    if ($query->max_num_pages > 1) {
+      $current_page = max(1, get_query_var('paged'));
+      echo '<nav class="pagination">';
+      echo paginate_links(array(
+          'base' => get_pagenum_link(1) . '%_%',
+          'format' => __('page', self::$instance->get_textdomain()) . '/%#%',
+          'current' => $current_page,
+          'total' => $query->max_num_pages,
+          'prev_text' => __('Prev', self::$instance->get_textdomain()),
+          'next_text' => __('Next', self::$instance->get_textdomain()),
+          'type' => 'list'
+      ));
+      echo '</nav>';
+    }
+  }
+
+  /**
+   * Hooks to WP init()
+   */
+  function init() {
+
+    if (is_admin()) {
+      
+    }
+    else {
+      $this->dev_tools();
+    }
+  }
+
+  /**
+   * Sets theme defaults
+   * 
+   * @return void
+   */
+  function setup() {
+    load_theme_textdomain($this->slug, get_template_directory() . '/lang');
+    add_editor_style('css/editor-style.css');
+    add_action('wp_enqueue_scripts', array(&$this, 'bulletproof_jquery'), 20);
+    add_filter('wp_page_menu_args', array(&$this, 'page_menu_args'));
+    add_filter('user_contactmethods', array(&$this, 'update_contact_methods'), 10, 1);
+    $this->cleanup_header();
+    $this->cleanup_nav();
+    add_filter('excerpt_length', array(&$this, 'get_excerpt_length'));
+    add_filter('excerpt_more', array(&$this, 'auto_excerpt_more'));
+    add_filter('get_the_excerpt', array(&$this, 'custom_excerpt_more'));
+    add_action('admin_bar_menu', array(&$this, 'dev_toolbar_items'), 100);
+    add_filter('template_include', array(&$this, 'dev_template_id'), 1000);
+  }
+
+  /**
+   * Clean up the header
+   * 
+   * @return void
+   */
+  function cleanup_header() {
+    remove_action('wp_head', 'wp_generator');
+    remove_action('wp_head', 'feed_links', 2);
+    remove_action('wp_head', 'feed_links_extra', 3);
+    remove_action('wp_head', 'rsd_link');
+    remove_action('wp_head', 'wlwmanifest_link');
+    remove_action('wp_head', 'index_rel_link');
+    remove_action('wp_head', 'parent_post_rel_link', 10, 0);
+    remove_action('wp_head', 'start_post_rel_link', 10, 0);
+    remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+    remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+    remove_action('wp_head', 'feed_links_extra', 2);
+    remove_action('wp_head', 'feed_links_extra', 3);
+    add_filter('use_default_gallery_style', '__return_false');
+    add_filter('style_loader_tag', array(&$this, 'cleanup_type'));
+    add_filter('wp_title', array(&$this, 'filter_wp_title'), 10, 2);
+    add_action('widgets_init', array(&$this, 'remove_recent_comments_style'));
+  }
+
+  /**
+   * Cleans up the default output of WP menus
+   * 
+   * @return void
+   */
+  function cleanup_nav() {
+    add_filter('wp_nav_menu', array(&$this, 'cleanup_nav_menu'));
+    add_filter('wp_page_menu', array(&$this, 'cleanup_nav_menu'));
+  }
+
+  /**
+   * Includes additional widgets
+   * 
+   * @return void
+   */
+  function register_widgets() {
+    require_once(locate_template('/lib/classes/widgets.php'));
+    register_widget('TabulaRasa_Twitter_Widget');
+  }
+
+  /**
+   * Gets the excerpt length from settings
+   * 
+   * @since Tabula Rasa 1.1
+   * @return int
+   */
+  function get_excerpt_length() {
+    return (int) $this->settings['excerpt_length'];
+  }
+
+  /**
+   * Replaces the "..." appended to post excerpts with an ellipsis
+   * 
+   * @since Tabula Rasa 1.1
+   * @param string $more
+   * @return string
+   */
+  function auto_excerpt_more($more) {
+    return ' &hellip;' . $this->continue_reading_link();
+  }
+
+  /**
+   * Returns a "Continue Reading" link for excerpts
+   *
+   * @since Tabula Rasa 1.1
+   * @return string "Continue Reading" link
+   */
+  function continue_reading_link() {
+    return ' <a href="' . get_permalink() . '">' . $this->settings['continue_reading_link'] . '</a>';
+  }
+
+  /**
+   * Adds a pretty "Continue Reading" link to custom post excerpts.
+   *
+   * @since Tabula Rasa 1.1
+   * @return string Excerpt with a pretty "Continue Reading" link
+   */
+  function custom_excerpt_more($output) {
+    if (has_excerpt() && !is_attachment()) {
+      $output .= $this->continue_reading_link();
+    }
+    return $output;
+  }
+
+  /**
+   * Replaces WordPress' built-in jQuery from the Google CDN
+   * 
+   * @return void
+   */
+  function bulletproof_jquery() {
+    global $wp_scripts;
+
+    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? 'https' : 'http';
+    $jquery_version = $wp_scripts->registered['jquery']->ver;
+    $url = $protocol . '://ajax.googleapis.com/ajax/libs/jquery/' . $jquery_version . '/jquery.min.js';
+    wp_deregister_script('jquery');
+    if (get_transient('google_jquery') == true) {
+      wp_register_script('jquery', $url, array(), $jquery_version, true);
+    }
+    else {
+      $resp = wp_remote_head($url);
+      if (!is_wp_error($resp) && 200 == $resp['response']['code']) {
+        set_transient('google_jquery', true, 60 * 5);
+        wp_register_script('jquery', $url, array(), $jquery_version, true);
+      }
+      else {
+        set_transient('google_jquery', false, 60 * 5);
+        $url = get_bloginfo('wpurl') . '/wp-includes/js/jquery/jquery.js';
+        wp_register_script('jquery', $url, array(), $jquery_version, true);
+      }
+    }
+    wp_enqueue_script('jquery');
+  }
+
+  /**
+   * Filters the default output of wp_title()
+   * 
+   * @global int $paged
+   * @global int $page
+   * @param string $title
+   * @param string $separator
+   * @return string
+   */
+  function filter_wp_title($title, $separator) {
+
+    if (is_feed()) {
+      return $title;
+    }
+
+    global $paged, $page;
+
+    $site_name = get_bloginfo('name', 'display');
+    $site_description = get_bloginfo('description', 'display');
+
+    if (is_search()) {
+      $title = sprintf(__('Search results for %s', $this->slug), '"' . get_search_query() . '"');
+      if ($paged >= 2)
+        $title .= ' ' . $separator . ' ' . sprintf(__('Page %s', $this->slug), $paged);
+      $title .= ' ' . $separator . ' ' . $site_name;
+      return $title;
+    }
+
+    if ($site_description && (is_home())) {
+      $title .= $site_description . ' ' . $separator;
+    }
+    if ($paged >= 2 || $page >= 2) {
+      $title .= ' ' . $separator . ' ' . sprintf(__('Page %s', $this->slug), max($paged, $page));
+    }
+    $title .= ' ' . $site_name;
+    return $title;
+  }
+
+  /**
+   * Strip the unnecessary type attribute from <style> elements
+   * 
+   * @param string $src
+   * @return string
+   */
+  function cleanup_type($src) {
+    return str_replace("type='text/css'", '', $src);
+  }
+
+  /**
+   * Removes the default styles that are packaged with the Recent Comments widget.
+   * 
+   * @return void
+   */
+  function remove_recent_comments_style() {
+    global $wp_widget_factory;
+    remove_action('wp_head', array(
+        $wp_widget_factory->widgets['WP_Widget_Recent_Comments'],
+        'recent_comments_style'
+            )
+    );
+  }
+
+  /**
+   * Show home link in wp_page_menu() by default
+   */
+  function page_menu_args($args) {
+    $args['show_home'] = true;
+    return $args;
+  }
+
+  /**
+   * Replaces the many default active menu classes with one
+   * 
+   * @param string $menu
+   * @return string
+   */
+  function cleanup_nav_menu($menu) {
+    $replace = array(
+        'current-menu-item' => 'active',
+        'current-menu-parent' => 'active',
+        'current-menu-ancestor' => 'active',
+        'current_page_item' => 'active',
+        'current_page_parent' => 'active',
+        'current_page_ancestor' => 'active',
+    );
+
+    $menu = str_replace(array_keys($replace), $replace, $menu);
+    return $menu;
+  }
+
+  /**
+   * Updates the user profile with some more up to date contact methods.
+   * 
+   * @param array $contact_methods
+   * @return array
+   */
+  function update_contact_methods($contact_methods) {
+    $new_methods = array(
+        'twitter' => 'Twitter',
+        'googleplus' => 'Google+',
+        'facebook' => 'Facebook',
+        'linkedin' => 'LinkedIn',
+        'appdotnet' => 'App.net'
+    );
+    foreach ($new_methods as $key => $value) {
+      if (!isset($contact_methods[$key])) {
+        $contact_methods[$key] = $value;
+      }
+    }
+    $remove_methods = array(
+        'aim',
+        'yim',
+        'jabber'
+    );
+    foreach ($remove_methods as $method) {
+      unset($contact_methods[$method]);
+    }
+
+    return $contact_methods;
+  }
+
+  /**
+   * Adds the dev menu to the toolbar
+   * 
+   * @since TabulaRasa 1.11
+   * @param object $admin_bar
+   */
+  function dev_toolbar_items($admin_bar) {
+    if (current_user_can('manage_options') && WP_DEBUG === true) {
+      $admin_bar->add_menu(array(
+          'id' => 'tr-dev-tools',
+          'title' => 'TabulaRasa Dev Tools',
+          'href' => '#',
+          'meta' => array(
+              'title' => __('Dev Tools'),
+          ),
+      ));
+      if (!is_admin()) {
+        global $wp_query;
+        $admin_bar->add_menu(
+                array(
+                    'id' => 'tr-dev-grid-overlay',
+                    'parent' => 'tr-dev-tools',
+                    'title' => __('Grid Overlay'),
+                    'href' => '#',
+                    'meta' => array(
+                        'title' => __('Show the grid overlay'),
+                    )
+                )
+        );
+        $admin_bar->add_menu(
+                array(
+                    'id' => 'tr-dev-current-template',
+                    'parent' => 'tr-dev-tools',
+                    'title' => __('Template') . ': ' . $this->dev['current_template'],
+                    'href' => admin_url('theme-editor.php?file=' . $this->dev['current_template'] . '&amp;theme=' . $this->theme->name),
+                )
+        );
+      }
+    }
+  }
+  
+  /**
+   * Stores the current template for later retrieval
+   * 
+   * Based on code submitted by Mike Bishop
+   * 
+   * @param string $template
+   * @return string
+   * 
+   */
+  function dev_template_id($template) {
+    $this->dev['current_template'] = basename($template);
+    return $template;
+  }
+
+  /**
+   * Loads the dev tools
+   * 
+   * @since TabulaRasa 1.11
+   */
+  function dev_tools() {
+    if (current_user_can('manage_options') && WP_DEBUG === true) {
+      add_action('wp_footer', array(&$this, 'dev_grid_overlay'), 100);
+      wp_register_style('tabularasa-dev', get_stylesheet_directory_uri() . '/css/dev.css');
+      wp_enqueue_style('tabularasa-dev');
+    }
+  }
+
+  /**
+   * Adds a grid overlay to the frontend to aid development
+   * 
+   * @since TabulaRasa 1.11
+   */
+  function dev_grid_overlay() {
+    ?>
+    <div class="dev-overlay">
+      <div class="container">
+        <div class="grid1 first">
+        </div>
+        <div class="grid1">
+        </div>
+        <div class="grid1">
+        </div>
+        <div class="grid1">
+        </div>
+        <div class="grid1">
+        </div>
+        <div class="grid1">
+        </div>
+        <div class="grid1">
+        </div>
+        <div class="grid1">
+        </div>
+        <div class="grid1">
+        </div>
+        <div class="grid1">
+        </div>
+        <div class="grid1">
+        </div>
+        <div class="grid1">
+        </div>
+      </div>
+    </div>
+    <script>
+      jQuery(document).ready(function() {
+        jQuery('#wp-admin-bar-tr-grid-overlay a').click(function() {
+          jQuery('.dev-overlay').toggle();
+        });
+      });
+    </script>
+    <?php
+  }
+
+}
---
+++ lib/classes/options.php
@@ -0,0 +1,224 @@
+<?php
+
+/**
+ * Class to handle options
+ */
+class TabulaRasa_options {
+
+  /**
+   * Theme slug, used for textdomain
+   * 
+   * @var string 
+   */
+  public $slug;
+
+  /**
+   * Theme name
+   * 
+   * @var string 
+   */
+  public $name;
+
+  /**
+   * 
+   *
+   * @var string 
+   */
+  private $general_settings_key = 'TabulaRasa_general_settings';
+
+  /**
+   *
+   * @var string 
+   */
+  private $advanced_settings_key = 'TabulaRasa_advanced_settings';
+
+  /**
+   *
+   * @var string 
+   */
+  private $options_key = 'TabulaRasa_options';
+
+  /**
+   *
+   * @var array 
+   */
+  private $settings_tabs = array();
+
+  /**
+   * Constructor
+   */
+  function __construct($slug, $name) {
+
+    $this->slug = $slug;
+    $this->name = $name;
+    add_action('init', array(&$this, 'load_settings'));
+    add_action('admin_init', array(&$this, 'register_general_settings'));
+    // add_action('admin_init', array(&$this, 'register_advanced_settings'));
+    add_action('admin_menu', array(&$this, 'add_admin_menus'));
+  }
+
+  /**
+   * Load the settings
+   */
+  function load_settings() {
+    $this->general_settings = (array) get_option($this->general_settings_key);
+    $this->advanced_settings = (array) get_option($this->advanced_settings_key);
+    $this->general_settings = array_merge(array(
+        'excerpt_length' => '40',
+				'continue_reading_link' => __('Continue Reading', $this->slug)
+            ), $this->general_settings);
+    /* $this->advanced_settings = array_merge(array(
+        'advanced_option' => 'Advanced value'
+            ), $this->advanced_settings);
+     * 
+     */
+  }
+
+  /**
+   * Registers the settings for the general tab
+   */
+  function register_general_settings() {
+    $this->settings_tabs[$this->general_settings_key] = __('General', $this->slug);
+
+    register_setting($this->general_settings_key, $this->general_settings_key);
+    add_settings_section('section_general', __('General Theme Settings', $this->slug), array(&$this, 'section_general_desc'), $this->general_settings_key);
+    add_settings_field('excerpt_length', __('Excerpt Length', $this->slug), array(&$this, 'excerpt_length'), $this->general_settings_key, 'section_general');
+		add_settings_field('continue_reading_link', __('Continue Reading Link', $this->slug), array(&$this, 'continue_reading_link'), $this->general_settings_key, 'section_general');
+  }
+
+  /**
+   * Description for the general settings tab
+   */
+  function section_general_desc() {
+    ?><p><?php _e('Some general theme settings.', $this->slug); ?></p><?php
+
+  }
+
+  /**
+   * Input for excerpt length
+   */
+  function excerpt_length() {
+    ?>
+    <input id="<?php echo $this->general_settings_key; ?>[excerpt_length]" class="regular-text" type="text" name="<?php echo $this->general_settings_key; ?>[excerpt_length]" value="<?php echo esc_attr($this->general_settings['excerpt_length']); ?>" />
+    <?php
+  }
+
+	  /**
+   * Input for excerpt length
+   */
+  function continue_reading_link() {
+    ?>
+    <input id="<?php echo $this->general_settings_key; ?>[continue_reading_link]" class="regular-text" type="text" name="<?php echo $this->general_settings_key; ?>[continue_reading_link]" value="<?php echo esc_attr($this->general_settings['continue_reading_link']); ?>" />
+    <?php
+  }
+  /**
+   * Register the settings for the advanced tab
+   */
+  function register_advanced_settings() {
+    $this->settings_tabs[$this->advanced_settings_key] = __('Advanced', $this->slug);
+
+    register_setting($this->advanced_settings_key, $this->advanced_settings_key);
+    add_settings_section('section_advanced', 'Advanced Plugin Settings', array(&$this, 'section_advanced_desc'), $this->advanced_settings_key);
+    add_settings_field('advanced_option', 'An Advanced Option', array(&$this, 'field_advanced_option'), $this->advanced_settings_key, 'section_advanced');
+  }
+
+  /**
+   * Adds a description to the advanced tab
+   */
+  function section_advanced_desc() {
+    echo 'Advanced section description goes here.';
+  }
+
+  /**
+   * Adds options for the advanced tab
+   */
+  function field_advanced_option() {
+    ?>
+    <input type="text" name="<?php echo $this->advanced_settings_key; ?>[advanced_option]" value="<?php echo esc_attr($this->advanced_settings['advanced_option']); ?>" />
+    <?php
+  }
+
+  /**
+   * Adds a menu entry for the theme options page
+   * 
+   * @return void
+   */
+  function add_admin_menus() {
+    $theme_page = add_theme_page(
+            __('Theme Options', $this->slug), __('Theme Options', $this->slug), 'edit_theme_options', 'theme_options', array(&$this, 'options_page')
+    );
+    if (!$theme_page) {
+      return;
+    }
+
+    add_action("load-$theme_page", array(&$this, 'options_help'));
+  }
+
+  /**
+   * Outputs the theme options page
+   */
+  function options_page() {
+    $tab = isset($_GET['tab']) ? $_GET['tab'] : $this->general_settings_key;
+    ?>
+    <div class="wrap">
+    <?php $this->options_tabs(); ?>
+      <form method="post" action="options.php">
+    <?php wp_nonce_field('update-options'); ?>
+      <?php settings_fields($tab); ?>
+      <?php do_settings_sections($tab); ?>
+        <?php submit_button(); ?>
+      </form>
+    </div>
+        <?php
+      }
+
+      /**
+       * Outputs the tabs for the theme options page
+       */
+      function options_tabs() {
+        $current_tab = isset($_GET['tab']) ? $_GET['tab'] : $this->general_settings_key;
+
+        screen_icon();
+        echo '<h2 class="nav-tab-wrapper">';
+        foreach ($this->settings_tabs as $tab_key => $tab_caption) {
+          $active = $current_tab == $tab_key ? 'nav-tab-active' : '';
+          echo '<a class="nav-tab ' . $active . '" href="?page=' . $this->options_key . '&tab=' . $tab_key . '">' . $tab_caption . '</a>';
+        }
+        echo '</h2>';
+      }
+
+      /**
+       * Outputs the help section for the theme options page
+       */
+      function options_help() {
+        $help = '<p>' . __('Some themes provide customization options that are grouped together on a Theme Options screen. If you change themes, options may change or disappear, as they are theme-specific. Your current theme, ' . $this->name . ', provides the following Theme Options:', $this->slug) . '</p>' .
+                '<ol>' .
+                '<li>' . __('<strong>Excerpt Length</strong>: You can choose the length (in characters) of the blog excerpt for your site.', $this->slug) . '</li>' .
+								'<li>' . __('<strong>Continue Reading Link</strong>: You can choose the text that will be displayed in the Continue Reading link in blog excerpts.', $this->slug) . '</li>' .
+                '</ol>' .
+                '<p>' . __('Remember to click "Save Changes" to save any changes you have made to the theme options.', $this->slug) . '</p>';
+
+        $sidebar = '<p><strong>' . __('For more information:', $this->slug) . '</strong></p>' .
+                '<p>' . __('<a href="http://codex.wordpress.org/Appearance_Theme_Options_Screen" target="_blank">Documentation on Theme Options</a>', $this->slug) . '</p>' .
+                '<p>' . __('<a href="http://wordpress.org/support/" target="_blank">Support Forums</a>', $this->slug) . '</p>';
+
+        $screen = get_current_screen();
+
+        if (method_exists($screen, 'add_help_tab')) {
+          // WordPress 3.3
+          $screen->add_help_tab(array(
+              'title' => __('Overview', $this->slug),
+              'id' => 'theme-options-help',
+              'content' => $help,
+                  )
+          );
+
+          $screen->set_help_sidebar($sidebar);
+        }
+        else {
+          // WordPress 3.2
+          add_contextual_help($screen, $help . $sidebar);
+        }
+      }
+
+    }
