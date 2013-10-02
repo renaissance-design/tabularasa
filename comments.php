af9d01dd8abf9daa5eeb6ad83bc0c4aed2aef1ce
Chris Cox <chris@renaissance-design.net>
2013-10-02 20:55:16 +0100

    Removed twentyten comment callback.

--- comments.php
+++ comments.php
@@ -40,15 +40,7 @@
 <?php endif; // check for comment navigation ?>
 
 			<ol>
-				<?php
-					/* Loop through and list the comments. Tell wp_list_comments()
-					 * to use twentyten_comment() to format the comments.
-					 * If you want to overload this in a child theme then you can
-					 * define twentyten_comment() and that will be used instead.
-					 * See twentyten_comment() in twentyten/functions.php for more.
-					 */
-					wp_list_comments( array( 'callback' => 'twentyten_comment' ) );
-				?>
+				<?php wp_list_comments(); ?>
 			</ol>
 
 <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
